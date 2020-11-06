3c6b369d10e19fff0597fdd506689d535d0f2d57
Gabriel Dantas <gabrielmd123@gmail.com>
2020-11-06 12:01:40 -0300

    tweaks

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,3 +1,20 @@
 <template>
   <div> Jokes </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best site for worst dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
